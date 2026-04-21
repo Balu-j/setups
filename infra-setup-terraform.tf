3bd418e871afc78d7b896ce3741f7a84668c15b9
Balu-j <balajijanjanamm@gmail.com>
2026-04-22 00:00:15 +0530

    Create infra-setup-terraform.tf

---
+++ infra-setup-terraform.tf
@@ -0,0 +1,18 @@
+provider "aws" {
+region = "ap-south-1"
+}
+
+resource "aws_instance" "one" {
+  count                  = 5
+  ami                    = "ami-0e12ffc2dd465f6e4 "
+  instance_type          = "t2.micro"
+  key_name               = "mumbai"
+  vpc_security_group_ids = ["sg-0149e35d3c4ba4089"]
+  tags = {
+    Name = var.instance_names[count.index]
+  }
+}
+
+variable "instance_names" {
+  default = ["jenkins", "tomcat-1", "tomcat-2", "Monitoring server","nexus"]
+}
