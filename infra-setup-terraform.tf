f51b70bf4b64f6476ec9f218c0b11ffe29d6a23d
Balu-j <balajijanjanamm@gmail.com>
2026-04-22 00:21:33 +0530

    Update infra-setup-terraform.tf

--- infra-setup-terraform.tf
+++ infra-setup-terraform.tf
@@ -4,10 +4,10 @@ region = "ap-south-1"
 
 resource "aws_instance" "one" {
   count                  = 5
-  ami                    = "ami-0e12ffc2dd465f6e4 "
+  ami                    = "ami-0e12ffc2dd465f6e4"
   instance_type          = "t2.micro"
   key_name               = "mumbai"
-  vpc_security_group_ids = ["sg-0e12ffc2dd465f6e4"]
+  vpc_security_group_ids = ["sg-0149e35d3c4ba4089"]
   tags = {
     Name = var.instance_names[count.index]
   }
